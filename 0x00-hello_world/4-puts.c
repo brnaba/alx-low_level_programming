7487323c04c38bd8dcf43d189fdaec039f4b2322
brnaba <brnaba@live.com>
2023-08-10 15:01:36 -0700

    puts eighth update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
 * Description: the program use puts to print a string.
 *Return: Always (0) success
 */
-int main()
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle" );
-return 0;
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
