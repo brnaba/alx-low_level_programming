747ba61e1437bd4a9a9b47a042953cd5159669bb
brnaba <brnaba@live.com>
2023-08-10 12:28:43 -0700

    puts task with betty check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /**
-* main function print string and return value equal zero.
+* main - Entry point
+* Description: the program use puts to print a string.
+*Return: Always (0) success
 */
 int  main(void)
 {
-puts (" "Programming is like building a multilingual puzzle \n");
-return(0);
+puts(" "Programming is like building a multilingual puzzle");
+return(0);/* The function return value zero */
 }
