3f8a1241889550706e522b08207479bc246e1f89
brnaba <brnaba@live.com>
2023-08-11 07:38:05 -0700

    last digit task update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,14 +8,15 @@
 */
 int main(void)
 {
-int  n;
+int  n, y;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+y = n % 10;
 if (n > 5)
-printf("Last digit of n is %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, y);
 else if (n == 0)
-printf("Last digit of n is %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, y);
 else
-printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 return (0);
 }
