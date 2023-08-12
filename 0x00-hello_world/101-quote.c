3cbdb9c6d864c8227de1fd8b6f2c1eebbc1e17cb
brnaba <brnaba@live.com>
2023-08-12 15:14:44 -0700

    quote task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Description: This program use function 'putchar' to print a string.
+* Return: Always (1) error
+*/
+int main(void)
+{
+putchar('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+return (1);
+}
