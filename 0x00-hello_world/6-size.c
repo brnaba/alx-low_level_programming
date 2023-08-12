ec358bfaf7f7938301b54e5343a466db7753c8c2
brnaba <brnaba@live.com>
2023-08-12 14:03:40 -0700

    print size of various types update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int i;
 long int  l;
 long long int ll;
 float f;
-printf("Size of a char : %ld byte(s)", sizeof(ch));
-printf("Size of an int : %ld byte(s)", sizeof(i));
-printf("Size of a long int : %ld byte(s)", sizeof(l));
-printf("Size of a long long int : %ld byte(s)", sizeof(ll));
-printf("Size of a float : %ld byte(s)", sizeof(f));
+printf("Size of a char : %ld byte(s)\n", sizeof(ch));
+printf("Size of an int : %ld byte(s)\n", sizeof(i));
+printf("Size of a long int : %ld byte(s)\n", sizeof(l));
+printf("Size of a long long int : %ld byte(s)\n", sizeof(ll));
+printf("Size of a float : %ld byte(s)\n", sizeof(f));
 return (0);
 }
