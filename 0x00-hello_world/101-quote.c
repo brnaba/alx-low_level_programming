265fb9dcdb73bb804ec491ff5688ed681e62e8af
brnaba <brnaba@live.com>
2023-08-12 15:33:18 -0700

    quote task second update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-char ch[60] = "and that piece of art is useful" - Dora Korpar, 2015-10-19";
+char ch[60] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 putchar(ch);
 putchar('\n');
 return (1);
