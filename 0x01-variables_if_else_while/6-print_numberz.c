1948451fce31a5d0793831ae38847131769f6d52
brnaba <brnaba@live.com>
2023-08-12 02:34:47 -0700

    second task of print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Description: the program prints alphabet in lowercase .
+*Return: Always (0) success
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
