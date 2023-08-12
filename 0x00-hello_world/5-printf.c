49e4b551a628da437189d4f649a354254986cda2
brnaba <brnaba@live.com>
2023-08-12 12:52:01 -0700

    functionprintf task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
 /**
 * main - Entry point
-* Description: the program use puts to print a string.
+* Description: This program use function 'printf' to print a string.
 *Return: Always (0) success
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
