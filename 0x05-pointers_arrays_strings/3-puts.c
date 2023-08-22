f8d0d789da5a134fa482b2fce9c7747798d5b51c
brnaba <brnaba@live.com>
2023-08-22 16:23:17 -0700

    putstask update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,15 @@
-include "main.h"
-
+#include "main.h"
 /**
- * _puts - prints a string, followed by a new line.
- * @str: string to print
+ * _puts - function that prints a string, followed by a new line
+ * @str: declaration of *str and paramters for the function _puts
+ * Return: Always 0.
  */
-
 void _puts(char *str)
-
 {
-while (*str != '\0')
+int c;
+for (c = 0; str[c] != 0; c++)
 {
-_putchar(*str++);
+_putchar(str[c]);
 }
 _putchar('\n');
 }
