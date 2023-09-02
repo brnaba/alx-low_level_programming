dd84183014e2b9d0078c63052a00d89ab9ecd89c
barnaba <brnaba@live.com>
2023-09-01 19:13:02 -0700

    creat my own libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function print values
+ * @str: input value
+ * Return: str (Success)
+ */
+
+void _puts(char *str)
+
+{
+_puts("\"Programming is my love");
+return (str);
+
+}
